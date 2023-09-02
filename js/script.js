ea92bb29df30165e1b59a1fcb5dfc4acdc1b3993
cyanide <54261993+3xplOr3r@users.noreply.github.com>
2023-09-02 22:31:07 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ sr.reveal('.home-image', {delay:400 , origin: 'right'})
 sr.reveal('.about, .services, .contact, .btn, .copyright', {delay:200 , origin: 'bottom'}) 
 
 const typed = new Typed(' .multiple-text', {
-    strings: ['Frontend Developer' ,'Content Writer' , 'Programmer' , 'NaSa-HaCkEr],
+    strings: ['Frontend Developer' ,'Content Writer' , 'Programmer' , 'NaSa-HaCkEr'],
     typeSpeed: 60,
     backSpeed: 50,
     backDelay: 1000,
